1d160dc615648232e95b715f954d9193329d3601
oviyad <oviyad.19cse@kongu.edu>
2022-05-22 23:52:27 +0530

    addproducts

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import CheckNavbar from "./components/checkNavbar";
 import Footer from "./components/Footer";
 import HomeInfoCardContainer from "./components/homeInfoCardContainer";
-
+import "./css/home-cards.css";
 function Home() {
   return (
     <div>
--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -55,64 +55,72 @@ function Register() {
       });
   };
   return (
-    <form className="form1" onSubmit={registerMe}>
-      <h2>Register Form</h2>
-      <br />
-      <div className="d-flex flex-column">
-        <div className="form-group">
+    <>
+      <img
+        className="img-fluid col-md-12 bg obj-fit-cover"
+        style={{ minHeight: "100vh" }}
+        src="https://i.pinimg.com/originals/33/ef/8b/33ef8b9c0b902154a6cd4103a21275ef.jpg"
+        alt=""
+      />
+      <form className="form1" onSubmit={registerMe}>
+        <h2>Register Form</h2>
+        <br />
+        <div className="d-flex flex-column">
           <div className="form-group">
-            <label htmlFor="email" className="form-label m-2 h5">
-              Email
+            <div className="form-group">
+              <label htmlFor="email" className="form-label m-2 h5">
+                Email
+              </label>
+              <input
+                type="email"
+                name="mail"
+                placeholder="Email"
+                className="form-control"
+                value={user.mail}
+                onChange={changeMe}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password" className="form-label m-2 h5">
+                Password
+              </label>
+              <input
+                type="password"
+                name="psd"
+                placeholder="Password"
+                className="form-control"
+                value={user.psd}
+                onChange={changeMe}
+                required
+              />
+            </div>
+            <label htmlFor="address" className="form-label m-2 h5">
+              Address
             </label>
-            <input
-              type="email"
-              name="mail"
-              placeholder="Email"
+            <textarea
+              name="addr"
+              placeholder="Address"
               className="form-control"
-              value={user.mail}
+              value={user.addr}
               onChange={changeMe}
               required
             />
           </div>
-          <div className="form-group">
-            <label htmlFor="password" className="form-label m-2 h5">
-              Password
-            </label>
-            <input
-              type="password"
-              name="psd"
-              placeholder="Password"
-              className="form-control"
-              value={user.psd}
-              onChange={changeMe}
-              required
-            />
+          <button className="btn btn-primary my-3 h4">Sign Up</button>
+          <div
+            className="h5 text-center"
+            style={msg === "" ? {} : msg.includes("Success") ? success : fail}
+          >
+            {msg}
           </div>
-          <label htmlFor="address" className="form-label m-2 h5">
-            Address
-          </label>
-          <textarea
-            name="addr"
-            placeholder="Address"
-            className="form-control"
-            value={user.addr}
-            onChange={changeMe}
-            required
-          />
+          <hr />
+          <span>
+            Already have an account? | <Link to="/login">Sign in</Link>
+          </span>
         </div>
-        <button className="btn btn-primary my-3 h4">Sign Up</button>
-        <div
-          className="h5 text-center"
-          style={msg === "" ? {} : msg.includes("Success") ? success : fail}
-        >
-          {msg}
-        </div>
-        <hr />
-        <span>
-          Already have an account? | <Link to="/login">Sign in</Link>
-        </span>
-      </div>
-    </form>
+      </form>
+    </>
   );
 }
 
